70900b2a73de02ba6a0330067b4587ca00a3e816
Archy <archy.revera@protonmail.com>
2021-10-31 23:21:18 +0000

    Add Bucket parent class

---
+++ src/main/java/flowerstore/Bucket.java
@@ -0,0 +1,13 @@
+package flowerstore;
+
+import java.util.ArrayList;
+
+public interface Bucket {
+    ArrayList<FlowerPack> flowerPacks = new ArrayList<FlowerPack>();
+
+    public void addFlowerPacks(FlowerPack flowerPack);
+    public ArrayList<FlowerPack> getFlowerPacks();
+    public double getPrice();
+
+    public String getDescriptor();
+}
